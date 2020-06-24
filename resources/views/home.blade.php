d1e229e8a5cc87297a788ec9b356c210df9954ab
juanesquivelmendez <amadeusdigital@gmail.com>
2020-06-24 13:44:11 -0500

    correcciòn de error en imagen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content_header')
     <h1 class="text-center">Bienvenido al Escritorio del Administrador de WeDiscover</h1>
     <div class="text-center">
-        <img src="{{asset('img/logowediscovernew.png)}}" alt="Logo WeDiscover" style="max-width: 320px">
+        <img src="{{asset('img/logowediscovernew.png')}}" alt="Logo WeDiscover" style="max-width: 320px">
     </div>
 @stop
 
